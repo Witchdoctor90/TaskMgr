4216b5e7345d91c3741ed370a7e5a339d0c0cd30
Oleksandr Dovhan <70040765+Witchdoctor90@users.noreply.github.com>
2025-02-12 15:53:55 +0200

    Created base classes for requests

--- TaskMgr.Application/Interfaces/IRepository.cs
+++ TaskMgr.Application/Interfaces/IRepository.cs
@@ -7,7 +7,7 @@ public interface IRepository<TEntity> where TEntity : BaseEntity
 {
     public Task<TEntity?> GetById(object id);
     public Task<IEnumerable<TEntity>> GetAll();
-    public Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate); 
+    public Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
     public Task<TEntity> Add(TEntity entity);
     public Task<TEntity> Update(TEntity entity);
     public Task<TEntity> Delete(object id);
