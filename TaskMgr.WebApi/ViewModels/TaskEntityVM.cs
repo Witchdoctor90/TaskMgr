391ae2688d8b65915dfced7dc8357fa5c52c425e
Oleksandr Dovhan <70040765+Witchdoctor90@users.noreply.github.com>
2025-03-30 23:31:20 +0300

    Removed ignored files

---
+++ TaskMgr.WebApi/ViewModels/TaskEntityVM.cs
@@ -0,0 +1,8 @@
+namespace TaskMgr.WebApi.ViewModels;
+
+public class TaskEntityVM
+{
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+
+}
